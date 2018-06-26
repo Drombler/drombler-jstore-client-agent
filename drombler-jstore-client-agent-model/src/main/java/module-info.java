c01f8f43530e4005a3e8ccdd7a4919b61c801c8a
Florian Brunner <fbrunnerreg@gmx.ch>
2018-06-27 00:14:47 +0200

    clean-ups

--- drombler-jstore-client-agent-model/src/main/java/module-info.java
+++ drombler-jstore-client-agent-model/src/main/java/module-info.java
@@ -1,4 +1,4 @@
-module drombler.jstore.client.agent.model {
+module org.drombler.jstore.client.agent.model {
     requires com.fasterxml.jackson.annotation;
     exports org.drombler.jstore.client.agent.model;
     exports org.drombler.jstore.client.agent.model.json;
